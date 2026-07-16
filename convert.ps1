884ab20221d10e8bae4d7d3b98e6e29c655faace
Ayush Bansal <ayush.ahws@gmail.com>
2026-07-16 05:08:10 +0000

    feat: Convert admission form to PDF and Word DOCX formats

---
+++ convert.ps1
@@ -0,0 +1,12 @@
+﻿$word = New-Object -ComObject Word.Application
+$word.Visible = $false
+$doc = $word.Documents.Open("D:\ayush bansal\WEBSITE\01_WEBSITE_SOURCE\public\AHWS_Admission_Application_Form.html")
+
+# Save as DOCX
+$doc.SaveAs([ref]"D:\ayush bansal\WEBSITE\01_WEBSITE_SOURCE\public\AHWS_Admission_Application_Form.docx", [ref]16)
+
+# Save as PDF
+$doc.SaveAs([ref]"D:\ayush bansal\WEBSITE\01_WEBSITE_SOURCE\public\AHWS_Admission_Application_Form.pdf", [ref]17)
+
+$doc.Close()
+$word.Quit()
